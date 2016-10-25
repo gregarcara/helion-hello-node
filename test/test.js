dd8668e73445e971191e943567e143b1d659a03f
Gregory Arcara <greg.arcara@gmail.com>
2016-10-25 12:32:26 -0700

    Removing tests

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-var should = require("should");
-
-
-describe('trivial test', function() {
-	it('will all be ok', function() {
-		(5).should.be.exactly(5);
-	});
-});
